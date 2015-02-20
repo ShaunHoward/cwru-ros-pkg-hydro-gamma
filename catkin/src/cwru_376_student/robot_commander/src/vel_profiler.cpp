70783a2d64a9f1f58b4b0fa2be756283235f652e
Shaun Howard <smh150@case.edu>
2015-02-20 15:32:26 -0500

    Changes to rotate Jinx.

--- catkin/src/cwru_376_student/robot_commander/src/vel_profiler.cpp
+++ catkin/src/cwru_376_student/robot_commander/src/vel_profiler.cpp
@@ -579,9 +579,10 @@ int main(int argc, char **argv) {
     ros::NodeHandle nodeHandle; // get a ros nodehandle; standard yadda-yadda
     //create a publisher object that can talk to ROS and issue twist messages on named topic;
     // note: this is customized for stdr robot; would need to change the topic to talk to jinx, etc.
-    velocityPublisher = nodeHandle.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1);
-    //ros::Subscriber sub = nodeHandle.subscribe("odom", 1, odomCallback);
-    ros::Subscriber sub = nodeHandle.subscribe("/robot0/odom", 1, odomCallback);
+    velocityPublisher = nodeHandle.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    //velocityPublisher = nodeHandle.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1);
+    ros::Subscriber sub = nodeHandle.subscribe("odom", 1, odomCallback);
+    //ros::Subscriber sub = nodeHandle.subscribe("/robot0/odom", 4, odomCallback);
 
     ros::Subscriber ping_dist_subscriber = nodeHandle.subscribe("lidar_dist", 1, pingDistanceCallback);
     ros::Subscriber lidar_alarm_subscriber = nodeHandle.subscribe("lidar_alarm", 1, lidarAlarmCallback);
@@ -590,16 +591,16 @@ int main(int argc, char **argv) {
 
     ros::Rate rTimer(1 / changeInTime); // frequency corresponding to chosen sample period DT; the main loop will run this fast
 
+    //initializeNewMove(rTimer);
+    //moveOnSegment(velocityPublisher, rTimer, 4.76); //4.75
+    initializeNewMove(rTimer);
     rotateToPhi(velocityPublisher, rTimer, -1.57);
-    //moveOnSegment(velocityPublisher, rTimer, 25); //4.75
-    //    initializeNewMove(rtimer);
-    //    moveOnSegment(velocityPublisher, rtimer, 0.0, -.314, -1.57);
-    //    initializeNewMove(rtimer);
-    //   moveOnSegment(velocityPublisher, rtimer, 12.3, 0.0, 0.0);
-    //   initializeNewMove(rtimer);
-    //    moveOnSegment(velocityPublisher, rtimer, 0.0, -.314, -1.57);
-    //    initializeNewMove(rtimer);
-    //    moveOnSegment(velocityPublisher, rtimer, 12.3, 0.0, 0.0);
+    //initializeNewMove(rTimer);
+    //moveOnSegment(velocityPublisher, rTimer, 12.3);
+    //initializeNewMove(rTimer);
+    //rotateToPhi(velocityPublisher, rTimer, -1.56);
+    //initializeNewMove(rTimer);
+    //moveOnSegment(velocityPublisher, rTimer, 12.3);
     return 0;
 }
 
