bddf9ec991992c473acc5e440cae96e2d14da14d
K <kxh337@case.edu>
2015-03-27 00:02:40 -0400

    fixed the points for glenan path on gazebo

--- catkin/src/cwru_376_student/des_state_generator/src/path_sender.cpp
+++ catkin/src/cwru_376_student/des_state_generator/src/path_sender.cpp
@@ -63,26 +63,26 @@ int main(int argc, char **argv) {
     vertex.header.frame_id = "map"; // specify this, so tf will know how to transform it
     // fill in the interesting data: (x,y) and phi = location and heading
     //vertex 1:
-    x=1.0;
-    y=2.0;
-    phi=0.123;
-    ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
-    vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
-    path_message.request.path.poses.push_back(vertex);
+    //x=1.0;
+    //y=2.0;
+    //phi=0.123;
+    //ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
+    //vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
+    //path_message.request.path.poses.push_back(vertex);
     //vertex 2:
-    x=3.0;
-    y=4.0;
-    phi=0.567;
-    ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
-    vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
-    path_message.request.path.poses.push_back(vertex);
+    //x=3.0;
+    //y=4.0;
+    //phi=0.567;
+    //ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
+    //vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
+    //path_message.request.path.poses.push_back(vertex);
     //vertex 3:
-    x=5.0;
-    y=6.0;
-    phi=2.345;
-    ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
-    vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
-    path_message.request.path.poses.push_back(vertex);
+    //x=5.0;
+    //y=6.0;
+    //phi=2.345;
+    //ROS_INFO("vertex: x,y,phi = %f, %f %f",x,y,phi);
+    //vertex.pose = xyPhi2Pose(x,y,phi); //x,y,phi
+    //path_message.request.path.poses.push_back(vertex);
 
 
     // fill in the interesting data: (x,y) and phi = location and heading
@@ -138,27 +138,33 @@ int main(int argc, char **argv) {
     //vertex 1
     //x = -3.452093;
     //y = -3.270599;
-    //phi = -1.5; //this is just the end rotation we want
-    //ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
-    //vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
-    //path_message.request.path.poses.push_back(vertex);
+    x = 4.75539;
+    y = 0;
+    phi = -1.5; //this is just the end rotation we want
+    ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
+    vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
+    path_message.request.path.poses.push_back(vertex);
 
     //vertex 2
     //x = -12;
     //y = 5;
-    //phi = -1.5; //this is just the end rotation we want
-    //ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
-    //vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
-    //path_message.request.path.poses.push_back(vertex);
+    x = 4.75539;
+    y = -12.069;
+    phi = 0; //this is just the end rotation we want
+    ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
+    vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
+    path_message.request.path.poses.push_back(vertex);
     //transmit this path message:
 
     //vetex 3 
     //x = -5.96538;
     //y = 11.313902;
-    //phi = -1.5; //this is just the end rotation we want
-    //ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
-    //vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
-    //path_message.request.path.poses.push_back(vertex);
+    x = -3.97857;
+    y = -12.069;
+    phi = 0; //this is just the end rotation we want
+    ROS_INFO("vertex: x,y,phi = %f, %f %f", x, y, phi);
+    vertex.pose = xyPhi2Pose(x, y, phi); //x,y,phi
+    path_message.request.path.poses.push_back(vertex);
 
     if (client.call(path_message)) {
         ROS_INFO("got ack from server");
